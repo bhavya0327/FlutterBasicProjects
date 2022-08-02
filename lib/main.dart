1f5b716a9ac29c5e3adf23b12a57d5cbcdb7fe36
bhavya0327 <99274860+bhavya0327@users.noreply.github.com>
2022-08-02 16:42:42 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'ui/home.dart';
 
 // void main() => runApp(const ScaffoldExample());
 
+
+// You just have to change the name after home: ..... to the name of the app you want to run
 void main() => runApp(new MaterialApp(
-  home: QuotesDaily(),
+  home: QuotesDaily(),                  //if home: BizCard(), then BusinessCard App will run
 ));
